b66bb04d877c2af647f16e10efb9967a4e0d0811
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-07-04 16:51:32 +0200

    First commit

---
+++ src/com/gabboman/fucklagg/FuckLagg.java
@@ -0,0 +1,14 @@
+package com.gabboman.fucklagg;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FuckLagg extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
